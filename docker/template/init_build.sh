94a2b1cb4c1b01274ac5fdb804d3a84cbce5cf3d
lentychang <lixis630@gmail.com>
2019-04-30 13:25:45 +0200

    update

--- docker/template/init_build.sh
+++ docker/template/init_build.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 echo usage: bash init_build.sh \[true\|false\]
-echo argv\[1\] ENABLE_NVIDIA to install nvidia driver, here nvidia-384
-enable_nvidia=$1
-docker build --build-arg ENABLE_NVIDIA=$enable_nvidia -t ros-docker-base:latest -f ros_base/Docker/Dockerfile . 
-docker build --build-arg ENABLE_NVIDIA=$enable_nvidia -t gazebo:base -f gazebo/Docker/Dockerfile.base . 
-docker build -t gazebo:ide -f gazebo/Docker/Dockerfile.ide . 
-docker build -t gazebo:latest -f gazebo/Docker/Dockerfile.latest . 
-docker build --build-arg ENABLE_NVIDIA=$enable_nvidia -t moveit:base -f moveit/Docker/Dockerfile.base . 
-docker build -t moveit:ide -f moveit/Docker/Dockerfile.ide . 
-docker build -t moveit:latest -f moveit/Docker/Dockerfile.latest . 
-docker build -t conda:python3.7 -f conda/Dockerfile.python3 .
-docker build --build-arg ENABLE_NVIDIA=$enable_nvidia -t pythonocc:base -f pythonocc/Docker/Dockerfile.base . 
-docker build -t pythonocc:ide -f pythonocc/Docker/Dockerfile.ide . 
-docker build -t pythonocc:latest -f pythonocc/Docker/Dockerfile.latest . 
-docker build --build-arg ENABLE_NVIDIA=$enable_nvidia -t pcl:base -f pcl/Docker/Dockerfile.base .
-docker build -t pcl:ide -f pcl/Docker/Dockerfile.ide .
-docker build -t pcl:latest -f pcl/Docker/Dockerfile.latest .
+echo argv\[1\] ENABLE_SUDO to install sudo
+enable_sudo=$1
+docker build --build-arg DOCKER_BASE_IMAGE=ubuntu:18.04 -f Dockerfile.base -t myenv.base .
+docker build --build-arg DOCKER_BASE_IMAGE=myenv.base \
+             --build-arg UID=1000 \
+             --build-arg GID=1000 \
+             --build-arg PW=docker \
+             --build-arg ENABLE_SUDO=true \
+             -f Dockerfile.user -t myenv:user .
+ 
