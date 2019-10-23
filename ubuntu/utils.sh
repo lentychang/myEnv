27dfd2fda80f98aa6cffb39739edfeb98e0a8b40
lentychang <lentychang@gmail.com>
2019-10-23 22:59:24 +0200

    add install chewing input method

--- ubuntu/utils.sh
+++ ubuntu/utils.sh
@@ -3,3 +3,8 @@ apt-get clean && \
     apt-get update && apt-get -y install \
     usbutils \
     pciutils
+
+# install chewing input method
+apt-get purge -y ibus && apt-get install -y fcitx fcitx-qimpanel fctix-chewing
+locale-gen zh_TW.UTF-8
+
