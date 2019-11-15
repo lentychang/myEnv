cc52fdfe08be5a5084b7ac23850686806eb4e228
Lenty Chang <lentychang@gmail.com>
2019-11-15 11:31:59 +0100

    update cxx dev

--- ubuntu/cxx_dev.sh
+++ ubuntu/cxx_dev.sh
@@ -13,22 +13,23 @@ apt-get update && apt-get install -y \
 apt-get update && 
 apt-get install -y ccache && \
 
-cd /usr/local && \
-mkdir bin-ccache && \
-cd bin-ccache && \
-ln -s $(which ccache) gcc && \
-ln -s $(which ccache) g++ && \
-ln -s $(which ccache) cpp && \
-ln -s $(which ccache) c++ && \
-ln -s $(which ccache) cc && \
+#cd /usr/local && \
+#mkdir bin-ccache && \
+#cd bin-ccache && \
+#ln -s $(which ccache) gcc && \
+#ln -s $(which ccache) g++ && \
+#ln -s $(which ccache) cpp && \
+#ln -s $(which ccache) c++ && \
+#ln -s $(which ccache) cc && \
 echo "export CCACHE_DIR=/tmp/.cache" >> ~/.bashrc && \
 echo "export set CC='ccache gcc'" >> ~/.bashrc && \
 echo "export set CXX='ccache g++'" >> ~/.bashrc && \
-echo "export PATH=/usr/local/bin-ccache:\$PATH" >> ~/.bashrc
+echo "export PATH=/usr/lib/ccache:\$PATH" >> ~/.bashrc
 
 echo "alias catkin_make='catkin_make -DCMAKE_C_COMPILER=/usr/local/bin-ccache/gcc -DCMAKE_CXX_COMPILER=/usr/local/bin-ccache/g++'" >> ~/.bashrc
 
 #install gold-linker to accelerate linking
+# since ubuntu 18, binutils-gold is put in binutils
 apt-get install -y binutils-gold && \
-update-alternatives --install "/usr/bin/ld" "ld" "/usr/bin/ld.gold" 20 && \
-update-alternatives --install "/usr/bin/ld" "ld" "/usr/bin/ld.bfd" 10
+update-alternatives --install "/usr/bin/ld" "ld" "/usr/bin//usr/bin/x86_64-linux-gnu-ld.gold" 20 && \
+update-alternatives --install "/usr/bin/ld" "ld" "/usr/bin//usr/bin/x86_64-linux-gnu-ld.bfd" 10
