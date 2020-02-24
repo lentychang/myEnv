8043063a5f86bba9e9a5be6b292e4f0c3db3e4ba
lentychang <lentychang@gmail.com>
2020-02-24 09:38:39 +0100

    update vscode,ubuntu utils

--- ubuntu/gnome_custimize_bionic.sh
+++ ubuntu/gnome_custimize_bionic.sh
@@ -2,6 +2,9 @@
 # method1
 gsettings set org.gnome.mutter workspaces-only-on-primary false
 # method2
-apt-get install gnome-tweak-tool
+apt-get install gnome-tweak-tool chrome-gnome-shell
+# install from this page
+# https://extensions.gnome.org/extension/1160/dash-to-panel/
+
   # configure from GUI
   gnome-tweaks
--- ubuntu/utils.sh
+++ ubuntu/utils.sh
@@ -2,8 +2,8 @@
 apt-get clean && \
     apt-get update && apt-get -y install \
     usbutils \
-    pciutils
-
+    pciutils \
+    nautilus-image-converter
 # install chewing input method
 apt-get purge -y ibus && apt-get install -y fcitx fcitx-qimpanel fctix-chewing
 locale-gen zh_TW.UTF-8
