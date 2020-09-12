4519c649565e1a7129fb076a2b7e4befa2d5a240
Chihpeng Chang <lentychang@gmail.com>
2020-09-12 18:02:02 +0200

    Update tmux_bash_history_fix.sh
    Add more description

--- ubuntu/tmux_bash_history_fix.sh
+++ ubuntu/tmux_bash_history_fix.sh
@@ -1,18 +1,26 @@
-# This is a script that seperates bash history based on tmux panel. For each tmux panel, it will save to different file.
+### Information ###
+# Last Modified Date: 2020/09/12
+# Maintainer: lentychang@gmail.com
+
+### Usage ###
+# Please append the following content into your ~/.bashrc
+
+### Purpose of this script ###
+# It seperates bash history based on tmux panel. For each tmux panel, it will save to different file.
 # The reason is that, timestamp is not really helpful if you keep switching tmux panels. For example, if the two panel is in different directory or repository.
 # You switch to master branch in panel 1, and switch to devel branch in panel 2. If you merge all history together, it is confusing.
 
-# number of command in History and History file size
+# number of commands in History file and History file size
 export HISTSIZE=100000
 export HISTFILESIZE=100000
 
 # Comment it, if you don't want timestamp in your history
 export HISTTIMEFORMAT="%F %T "
 
-# avoid duplicates..
+# Avoid duplicate command
 export HISTCONTROL=ignoredups:erasedups
 
-# append history entries..
+# Append history entries
 shopt -s histappend
 
 # After each command, save and reload history
